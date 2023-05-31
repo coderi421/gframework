f7caace72f5179106d8d02c9e0b23ad0949150ee
zhengtianyi <zhengtianyi@sinkatec.com>
2023-05-31 14:01:32 +0800

    refactor: decouple errors from gmicro package in the jwt logic

--- app/pkg/code/code_generated.go
+++ app/pkg/code/code_generated.go
@@ -6,6 +6,22 @@ package code
 
 // init register error codes defines in this source code to `imooc/mxshop/pkg/errors`
 func init() {
+	register(ErrEncrypt, 401, "Error occurred while encrypting the user password")
+	register(ErrSignatureInvalid, 401, "Signature is invalid")
+	register(ErrExpired, 401, "Token expired")
+	register(ErrInvalidAuthHeader, 401, "Invalid authorization header")
+	register(ErrMissingHeader, 401, "The `Authorization` header was empty")
+	register(ErrPasswordIncorrect, 401, "Password was incorrect")
+	register(ErrPermissionDenied, 403, "Permission denied")
+	register(ErrEncodingFailed, 500, "Encoding failed due to an error with the data")
+	register(ErrDecodingFailed, 500, "Decoding failed due to an error with the data")
+	register(ErrInvalidJSON, 500, "Data is not valid JSON")
+	register(ErrEncodingJSON, 500, "JSON data could not be encoded")
+	register(ErrDecodingJSON, 500, "JSON data could not be decoded")
+	register(ErrInvalidYaml, 500, "Data is not valid Yaml")
+	register(ErrEncodingYaml, 500, "Yaml data could not be encoded")
+	register(ErrDecodingYaml, 500, "Yaml data could not be decoded")
+	register(ErrDatabase, 500, "Database error")
 	register(ErrConnectDB, 500, "Init db error")
 	register(ErrConnectGRPC, 500, "Connect to grpc error")
 	register(ErrGoodsNotFound, 404, "Goods not found")
